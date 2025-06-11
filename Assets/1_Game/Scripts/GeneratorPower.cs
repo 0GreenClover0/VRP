e70d9dec69c8e92e0ecf8e4cade5acb65096b68e
MikeMG-PL <36279247+MikeMG-PL@users.noreply.github.com>
2025-06-11 14:47:42 +0200

    Generator: Replace model

--- Assets/1_Game/Scripts/GeneratorPower.cs
+++ Assets/1_Game/Scripts/GeneratorPower.cs
@@ -35,6 +35,9 @@ public class GeneratorPower : MonoBehaviour
     public Transform mechanism1;
     public Transform mechanism2;
     public Transform mechanism3;
+    public Transform mechanism4;
+    public Transform mechanism5;
+    public Transform mechanism6;
 
     // 0-100
     private float currentGeneratorPower;
@@ -106,7 +109,7 @@ public class GeneratorPower : MonoBehaviour
     void SetBarVisuals()
     {
         Vector3 scale = barTransform.localScale;
-        scale.x = AK.MapRangeClamped(barValue, 0.0f, 100.0f, 0.0f, 0.05f);
+        scale.x = AK.MapRangeClamped(barValue, 0.0f, 100.0f, 0.0f, 0.04f);
         barTransform.localScale = scale;
     }
 
@@ -115,13 +118,22 @@ public class GeneratorPower : MonoBehaviour
         Vector3 v1 = mechanism1.transform.localEulerAngles;
         Vector3 v2 = mechanism2.transform.localEulerAngles;
         Vector3 v3 = mechanism3.transform.localEulerAngles;
+        Vector3 v4 = mechanism4.transform.localEulerAngles;
+        Vector3 v5 = mechanism4.transform.localEulerAngles;
+        Vector3 v6 = mechanism4.transform.localEulerAngles;
 
         v1.z -= rotationDelta;
         v2.z += rotationDelta;
         v3.z -= rotationDelta;
+        v4.z += rotationDelta;
+        v5.z -= rotationDelta;
+        v6.z += rotationDelta;
 
         mechanism1.transform.localEulerAngles = v1;
         mechanism2.transform.localEulerAngles = v2;
         mechanism3.transform.localEulerAngles = v3;
+        mechanism4.transform.localEulerAngles = v4;
+        mechanism5.transform.localEulerAngles = v5;
+        mechanism6.transform.localEulerAngles = v6;
     }
 }
