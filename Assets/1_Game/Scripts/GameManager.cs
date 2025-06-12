983fe15e3ddb4d2307080b0c114236a81af58d81
MikeMG-PL <36279247+MikeMG-PL@users.noreply.github.com>
2025-06-12 18:43:09 +0200

    Sound: Add and implement them

--- Assets/1_Game/Scripts/GameManager.cs
+++ Assets/1_Game/Scripts/GameManager.cs
@@ -1,6 +1,27 @@
+using System;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject player;
+
+    [Space]
+    [Header("Global looping sounds")]
+    
+    public AudioClip windSound;
+
+    private AudioSource audioSource;
+    
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        
+        PlayWindSound();
+    }
+
+    void PlayWindSound()
+    {
+        audioSource.clip = windSound;
+        audioSource.Play();
+    }
 }
