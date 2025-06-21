94c0e9e02608e746a54ff638ccd960b7b0a54549
MikeMG-PL <36279247+MikeMG-PL@users.noreply.github.com>
2025-06-21 13:11:34 +0200

    PullSwitch: Add adjustable epic flash effect

--- Assets/1_Game/Scripts/PullSwitch.cs
+++ Assets/1_Game/Scripts/PullSwitch.cs
@@ -2,10 +2,17 @@ using System.Linq;
 using Oculus.Interaction;
 using UnityEngine;
 
+class FakeEnvironmentLightData
+{
+    public Color fogColor;
+    public float ambientIntensity;
+    public float skyExposure;
+    public float reflectionIntensity;
+}
+
 public class PullSwitch : MonoBehaviour
 {
     [SerializeField] private InteractableGroupView interactableGroupView;
-    [SerializeField] private Light pointLight;
     [SerializeField] private MaterialPropertyBlockEditor bulbMaterial;
     [SerializeField] private GameObject grabbableObject;
     [SerializeField] private Transform ropeDesiredPosition;
@@ -13,10 +20,29 @@ public class PullSwitch : MonoBehaviour
     [SerializeField] private AudioClip flashSound;
     [SerializeField] private SnapInteractable lightbulbSocket;
 
-    private float flashActiveVisual = 0.0f;
+    private float flashActiveVisual = -0.1f;
     private AudioSource audioSource;
     private MaterialPropertyColor defaultMaterialColor;
     private MaterialPropertyColor activatedMaterialColor;
+
+    [Space] [Header("Normal light data")]
+    [SerializeField] private Color normalFogColor = new Color(0.0f, 0.0f, 0.02f, 1.0f);
+    [SerializeField] private float normalAmbientIntensity = 0.3f;
+    [SerializeField] private float normalSkyExposure = 1.0f;
+    [SerializeField] private float normalReflectionIntensity = 0.275f;
+    
+    [Space] [Header("Flash light data")]
+    [SerializeField] private Color flashFogColor = new Color(0.7f, 0.7f, 1.0f, 1.0f);
+    [SerializeField] private float flashAmbientIntensity = 4.0f;
+    [SerializeField] private float flashSkyExposure = 3.0f;
+    [SerializeField] private float flashReflectionIntensity = 1.0f;
+    
+    [Space] [Header("Flash curves")]
+    [SerializeField] private AnimationCurve lightUp;
+    [SerializeField] private AnimationCurve lightDown;
+    
+    private FakeEnvironmentLightData normalLightData = new FakeEnvironmentLightData();   // LERP A
+    private FakeEnvironmentLightData flashLightData = new FakeEnvironmentLightData();    // LERP B
 
     private const float flashTime = 5.0f;
     private const float returningToDefaultSpeed = 0.1f;
@@ -38,14 +64,24 @@ public class PullSwitch : MonoBehaviour
         activatedMaterialColor = new MaterialPropertyColor
         {
             name = "_BaseColor",
-            value = Color.yellow
+            value = new Color(0.4f, 0.4f, 0.4f, 1.0f)
         };
+
+        normalLightData.fogColor = normalFogColor;
+        normalLightData.ambientIntensity = normalAmbientIntensity;
+        normalLightData.skyExposure = normalSkyExposure;
+        normalLightData.reflectionIntensity = normalReflectionIntensity;
+
+        flashLightData.fogColor = flashFogColor;
+        flashLightData.ambientIntensity = flashAmbientIntensity;
+        flashLightData.skyExposure = flashSkyExposure;
+        flashLightData.reflectionIntensity = flashReflectionIntensity;
     }
 
     private void Update()
     {
         isBulbInSocket = lightbulbSocket.SelectingInteractors.Count > 0;
-
+        
         if (isBulbInSocket)
         {
             // We always have only one interactor
@@ -63,41 +99,27 @@ public class PullSwitch : MonoBehaviour
             grabbableObject.transform.position = Vector3.MoveTowards(grabbableObject.transform.position, ropeDesiredPosition.position, Time.deltaTime * returningToDefaultSpeed);
         }
 
-        if (isBulbInSocket && flashActiveVisual <= 0.0f && interactableGroupView.SelectingInteractorsCount != 0 && grabbableObject.transform.position.y < yPositionActivateFlashTreshold.transform.position.y)
+        if (isBulbInSocket && interactableGroupView.SelectingInteractorsCount != 0 && grabbableObject.transform.position.y < yPositionActivateFlashTreshold.transform.position.y)
         {
             ActivateFlash();
             bulbMaterial.ColorProperties = new System.Collections.Generic.List<MaterialPropertyColor>() { activatedMaterialColor };
         }
 
-        if (flashActiveVisual <= 0.0f)
+        if (flashActiveVisual < 0.0f)
         {
             bulbMaterial.ColorProperties = new System.Collections.Generic.List<MaterialPropertyColor>() { defaultMaterialColor };
         }
 
-        if (flashActiveVisual >= 3.0f && flashActiveVisual <= 5.0f)
+        if (flashActiveVisual >= 0.0f && flashActiveVisual <= flashTime)
         {
-            if (!pointLight.enabled)
-            {
-                pointLight.enabled = true;
-            }
-
-            float t = Mathf.InverseLerp(5.0f, 3.0f, flashActiveVisual); // t goes from 0.0 to 1.0
-            float ramp = Mathf.Pow(t, 1.5f); // Faster ramp-up
-            float fade = Mathf.Pow(1.0f - t, 1.2f); // Slower fade-out
-            float shaped = ramp * fade * 4.0f; // Bell-like pulse shape
-            pointLight.intensity = shaped * 100.0f;
-        }
-        else
-        {
-            pointLight.intensity = 0.0f;
-
-            if (pointLight.enabled)
-            {
-                pointLight.enabled = false;
-            }
+            float flashCounter01 = AK.MapRangeClamped(flashActiveVisual, 0.0f, flashTime, 0.0f, 1.0f);
+            float x = Mathf.Lerp(flashTime, 0.0f, flashCounter01);
+            float y = lightDown.Evaluate(x);
+            LerpFakeLighting(y);
         }
 
-        flashActiveVisual -= Time.deltaTime;
+        flashActiveVisual -= Time.deltaTime / flashTime;
+        flashActiveVisual = Mathf.Clamp(flashActiveVisual, -1.0f, float.MaxValue);
     }
 
     [ContextMenu("Activate flash")]
@@ -114,6 +136,20 @@ public class PullSwitch : MonoBehaviour
         EjectLightbulb();
     }
 
+    // t: 0-1
+    void LerpFakeLighting(float t)
+    {
+        Color currentFogColor = Color.Lerp(normalLightData.fogColor, flashLightData.fogColor, t);
+        float currentAmbientIntensity = Mathf.Lerp(normalLightData.ambientIntensity, flashLightData.ambientIntensity, t);
+        float currentSkyExposure = Mathf.Lerp(normalLightData.skyExposure, flashLightData.skyExposure, t);
+        float currentReflectionIntensity = Mathf.Lerp(normalLightData.reflectionIntensity, flashLightData.reflectionIntensity, t);
+        
+        RenderSettings.fogColor = currentFogColor;
+        RenderSettings.ambientIntensity = currentAmbientIntensity;
+        RenderSettings.skybox.SetFloat("_Exposure", currentSkyExposure);
+        RenderSettings.reflectionIntensity = currentReflectionIntensity;
+    }
+    
     void EjectLightbulb()
     {
         Debug.Log(currentBulb);
