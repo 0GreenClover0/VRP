aa509978685ebd979e7826766344c0c7d8791a29
Mikolaj Przybylski <clovers02123@gmail.com>
2025-06-22 15:39:17 +0200

    GameOver: Add game over screen

--- Assets/1_Game/Scripts/GameManager.cs
+++ Assets/1_Game/Scripts/GameManager.cs
@@ -3,26 +3,46 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance { get; private set; }
+
     public GameObject player;
     public StoryController storyController;
 
+    [SerializeField] private GameOverScreen gameOver;
+
     [Space]
     [Header("Global looping sounds")]
-    
+
     public AudioClip windSound;
-	
+
     private AudioSource audioSource;
-    
+
     private void Awake()
     {
         audioSource = GetComponent<AudioSource>();
-        
+
         PlayWindSound();
+
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Debug.LogError("GameManager instance already existed. Destroying the old one.");
+            Destroy(Instance.gameObject);
+            Instance = this;
+        }
     }
 
     private void PlayWindSound()
     {
         audioSource.clip = windSound;
         audioSource.Play();
+    }
+
+    public void GameOver()
+    {
+        gameOver.Appear();
     }
 }
